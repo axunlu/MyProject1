781b3f1fedc8c8f29ce9fbac4ea421df8a48ca82
luozhenhong <1019502679@qq.com>
2024-09-05 08:51:36 +0800

    feat:新增预处理文件函数

--- 3122004748/main.py
+++ 3122004748/main.py
@@ -15,4 +15,15 @@ def read_file(file_path):
     return text
 
 
+'''
+    预处理文件
+'''
 
+
+def preprocess_text(text):
+    # 正则表达式，去掉文本字母、数字、下划线，空白字符
+    text = re.sub(r'[^\w\s]', '', text)
+    # 用jieba库划分句子
+    words = jieba.lcut(text)
+    # 最后拼接
+    return ' '.join(words)
