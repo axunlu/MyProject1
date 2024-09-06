58ac53caceb8bc88d87e41314e646e45abc3d257
luozhenhong <1019502679@qq.com>
2024-09-06 21:02:54 +0800

    feat:新增三条读文件测试用例

--- 3122004748/test_cases.py
+++ 3122004748/test_cases.py
@@ -31,6 +31,30 @@ class TestFunction:
             text = text.strip()
             assert float(text) == rounded_similarity, "写入的相似度与计算的相似度不一致"
 
+    def test_file01(self):
+        """
+        原文文件路径存在（预期：正常打开）
+        """
+        original_file_path = r"D:\pythonProject\MyProject1\3122004748\examples\orig.txt"
+        text = main.read_file(original_file_path)
+        assert text is not None
+
+    def test_file02(self):
+        """
+        原文文件路径不存在（预期：抛出异常FileNotFoundError，提示文件路径不存在）
+        """
+        original_file_path = r"D:\pythonProject\MyProject1\3122004748\examples\345.txt"
+        exception_name = main.read_file(original_file_path)
+        assert exception_name == "FileNotFoundError"
+
+    def test_file03(self):
+        """
+        抄袭版文件内容为空（预期：提示文件为空）
+        """
+        plagiarized_file_path = r"D:\pythonProject\MyProject1\3122004748\examples\orig_empty.txt"
+        exception_name = main.read_file(plagiarized_file_path)
+        assert exception_name == "EmptyFile"
+
 
 if __name__ == '__main__':
     pytest.main(['-vs', 'test_cases.py'])
